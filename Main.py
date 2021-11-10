cc2623cafc93aa0b5c179ee4e5c4d3bbb45470f3
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-11-10 15:11:22 +0530

    import easygui library

--- Main.py
+++ Main.py
@@ -1,4 +1,5 @@
 # This is the official code section of Image Cartoonifier
 # Author: Manish Sharma
 
-import cv2  #importing computer vision library for image processing
+import cv2        #importing computer vision library for image processing
+import easygui   #importing easygui library to open the filebox
