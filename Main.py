4619a85b2d2567c3e0a82ca0c8e1587528fcf43c
manish2407 <manish.sharma_cs18@gla.ac.in>
2021-11-10 15:13:31 +0530

    import numpy

--- Main.py
+++ Main.py
@@ -1,5 +1,6 @@
 # This is the official code section of Image Cartoonifier
 # Author: Manish Sharma
 
-import cv2        #importing computer vision library for image processing
-import easygui   #importing easygui library to open the filebox
+import cv2            #importing computer vision library for image processing
+import easygui        #importing easygui library to open the filebox
+import numpy as np    #to store image
